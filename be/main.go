82abb4263ff77b9d45d91f90a3ae2f792c45690a
Vanhhhhhhhh <110473289+Vanhhhhhhhh@users.noreply.github.com>
2025-05-19 14:21:32 +0700

    function New() to creat new instance with data from env file

--- be/main.go
+++ be/main.go
@@ -2,25 +2,10 @@ package main
 
 import (
 	"be/db"
-	"log"
-	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
-
-	sql := &db.Sql{
-		Host:     os.Getenv("DB_HOST"),
-		Port:     os.Getenv("DB_PORT"),
-		UserName: os.Getenv("DB_USER"),
-		Password: os.Getenv("DB_PASSWORD"),
-		DbName:   os.Getenv("DB_NAME"),
-	}
+	sql := db.New()
 	sql.Connect()
 	defer sql.Close()
 }
