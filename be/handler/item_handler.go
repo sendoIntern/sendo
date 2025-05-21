56b7cc884ed1a7d4ebf8af4c659b8c0c82ad8743
Vanhhhhhhhh <110473289+Vanhhhhhhhh@users.noreply.github.com>
2025-05-21 10:18:53 +0700

    done API create item

--- be/handler/item_handler.go
+++ be/handler/item_handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"be/db"
+	"be/dto"
 	"be/entity"
 	"fmt"
 	"net/http"
@@ -9,7 +10,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func GetItemsHandler(c *gin.Context){
+func GetItemsHandler(c *gin.Context) {
 	var items []entity.Item
 	result := db.DB.Find(&items)
 	if result.Error != nil {
@@ -19,7 +20,7 @@ func GetItemsHandler(c *gin.Context){
 	c.JSON(200, items)
 }
 
-func GetItemByIdHandler(c *gin.Context){
+func GetItemByIdHandler(c *gin.Context) {
 	var item entity.Item
 	id := c.Param("itemId")
 	result := db.DB.First(&item, "id = ?", id)
@@ -27,18 +28,48 @@ func GetItemByIdHandler(c *gin.Context){
 		c.JSON(http.StatusBadRequest, gin.H{"error": result.Error.Error()})
 		return
 	}
-	db.DB.Model(&item).Update("view", item.View + 1)
+	db.DB.Model(&item).Update("view", item.View+1)
 	c.JSON(http.StatusOK, item)
-} 
-
-func CreateItemHandler(c *gin.Context){
-	fmt.Println("CreateItemHandler")
 }
 
-func DeleteItemHandler(c *gin.Context){
+func CreateItemHandler(c *gin.Context) {
+	var req dto.ItemCreationRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	item := entity.Item{
+		Name:        req.Name,
+		Description: req.Description,
+		Quantity:    req.Quantity,
+		Price:       req.Price,
+		Picture:     req.Picture,
+	}
+
+	if err := db.DB.Create(&item).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Cannot create item"})
+		return
+	}
+
+	resp := dto.ItemCreationResponse{
+		ID:          item.ID,
+		Name:        item.Name,
+		Description: item.Description,
+		Quantity:    item.Quantity,
+		Price:       item.Price,
+		Picture:     item.Picture,
+		CreateAt:    item.CreatedAt,
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Item created", "item": resp})
+}
+
+func DeleteItemHandler(c *gin.Context) {
 	fmt.Println("DeleteItemHandler")
 }
 
-func UpdateItemByIdHandler(c *gin.Context){
+func UpdateItemByIdHandler(c *gin.Context) {
 	fmt.Println("UpdateItemByIdHandler")
 }
