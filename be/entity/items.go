74a16aae1d8cf18e890cde1a8e32fff45290d4d5
Vanhhhhhhhh <110473289+Vanhhhhhhhh@users.noreply.github.com>
2025-05-21 09:40:40 +0700

    fix conflict

--- be/entity/items.go
+++ be/entity/items.go
@@ -8,7 +8,6 @@ import (
 )
 
 type Item struct {
-<<<<<<< HEAD
 	ID          uuid.UUID `json:"id" gorm:"type:uuid;primaryKey"`
 	Name        string    `json:"name" gorm:"unique;not null"`
 	Description string    `json:"description" gorm:"type:text"`
@@ -19,18 +18,6 @@ type Item struct {
 	Recommend   int       `json:"recommend" gorm:"default:0"`
 	CreateAt    time.Time `json:"create_at"`
 	UpdateAt    time.Time `json:"update_at"`
-=======
-	ID          uuid.UUID `gorm:"type:uuid;primaryKey"`
-	Name        string    `gorm:"unique;not null"`
-	Description string    `gorm:"type:text"`
-	Quantity    int       `gorm:"default:0"`
-	Price       float64   `gorm:"default:0"`
-	Picture     string
-	View        int64 	  `gorm:"default:0"`
-	Recommend   int 	  `gorm:"default:0"`
-	CreateAt    time.Time
-	UpdateAt    time.Time
->>>>>>> 52b5ed32b4751e7b2a0183ee5d9db24c52474700
 }
 
 // auto generate uuid before save a new user into database
