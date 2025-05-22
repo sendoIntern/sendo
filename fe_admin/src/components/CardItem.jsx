558493e622aff86ac15c8350c765f9e4a7ad4050
BUICUONGG <cuongbb14@gmail.com>
2025-05-22 10:04:18 +0700

    dang lam

--- fe_admin/src/components/CardItem.jsx
+++ fe_admin/src/components/CardItem.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Button, Card } from "antd";
+import { Button, Card, Modal, Tooltip } from "antd";
 import { axiosInstance } from "../lib/axios";
 import Meta from "antd/es/card/Meta";
+import { useNavigate } from "react-router-dom";
 
 function CardItem() {
   const [data, setData] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -21,28 +25,74 @@ function CardItem() {
     fetchProducts();
   }, []);
 
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
     <div
       style={{
         display: "flex",
-        justifyItems: "space-between",
         flexWrap: "wrap",
+        justifyContent: "space-around",
+        gap: "20px",
       }}
     >
-      {data.map((product) => (
+      {data.map((item) => (
         <Card
-          key={product.ID}
+          key={item.id}
           style={{ width: 300 }}
-          cover={<img alt="Not Found" src={product.Picture} />}
+          cover={<img alt="example" src={item.picture} onClick={showModal} />}
         >
-          <Meta title={product.Name} description={product.Description} />
-          <p>Price: {product.Price}</p>
-          <p>Recommend: {product.Recommend}</p>
-          <Button style={{ marginTop: "10px", width: "100%" }} type="primary">
+          <Meta
+            style={{ textAlign: "center" }}
+            title={item.name}
+            onClick={showModal}
+          />
+          <Meta
+            style={{ height: "100px" }}
+            description={`${item.description}`}
+            onClick={showModal}
+          />
+          <Meta description={`Price: ${item.price}`} onClick={showModal} />
+          <Button style={{ width: "100%", marginTop: "10px", color: "blue" }}>
             Buy
           </Button>
         </Card>
       ))}
+      {/* <Modal
+        title="Basic Modal"
+        closable={{ "aria-label": "Custom Close Button" }}
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Card
+          key={item.id}
+          style={{ width: 300 }}
+          cover={<img alt="example" src={item.picture} onClick={showModal} />}
+        >
+          <Meta
+            style={{ textAlign: "center" }}
+            title={item.name}
+            onClick={showModal}
+          />
+          <Meta
+            style={{ height: "100px" }}
+            description={`${item.description}`}
+            onClick={showModal}
+          />
+          <Meta description={`Price: ${item.price}`} onClick={showModal} />
+          <Button style={{ width: "100%", marginTop: "10px", color: "blue" }}>
+            Buy
+          </Button>
+        </Card>
+      </Modal> */}
     </div>
   );
 }
