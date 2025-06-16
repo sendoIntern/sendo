0e481081d85df72dec5dbeaa7d4e6ac4c633d560
Vanhhhhhhhh <110473289+Vanhhhhhhhh@users.noreply.github.com>
2025-06-16 11:33:59 +0700

    API response struct

---
+++ be/services/items/model/response/apiResponse.go
@@ -0,0 +1,8 @@
+package response
+
+type APIResponse struct {
+	Status  string      `json:"status"`
+	Message string      `json:"message,omitempty"`
+	Data    interface{} `json:"data,omitempty"`
+	Error   string      `json:"error,omitempty"`
+}
