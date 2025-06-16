4d4433a56768ac6e5086e2fac1510d9157d0a5af
Vanhhhhhhhh <110473289+Vanhhhhhhhh@users.noreply.github.com>
2025-06-16 15:04:22 +0700

    API response struct

--- be/services/items/model/response/apiResponse.go
+++ be/services/items/model/response/apiResponse.go
@@ -1,8 +1,11 @@
 package response
 
+import "be/pkg/pagination"
+
 type APIResponse struct {
-	Status  string      `json:"status"`
-	Message string      `json:"message,omitempty"`
-	Data    interface{} `json:"data,omitempty"`
-	Error   string      `json:"error,omitempty"`
+	Status     string            `json:"status"`
+	Message    string            `json:"message,omitempty"`
+	Pagination pagination.Paging `json:"pagination,omitempty"`
+	Data       interface{}       `json:"data,omitempty"`
+	Error      string            `json:"error,omitempty"`
 }
